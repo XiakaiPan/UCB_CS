61931aff5bd3eecd38fd96cc12d711c4face174f
XiakaiPan <13212017962@163.com>
2020-03-25 00:55:01 +0800

    24日进度缓慢，Hog实验还未开始

---
+++ CS61A/2020年3月24日/Control_3/lab01/lab01_extra.py
@@ -0,0 +1,49 @@
+"""Optional questions for Lab 1"""
+
+# While Loops
+
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    product = 1
+    while k:
+       product *= n
+       n = n - 1
+       k = k - 1
+    return product
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    pre, cur = 0, 0
+    while n:
+        pre = n % 10
+        n //= 10
+        cur = n % 10
+        if pre == 8 and cur == 8:
+            return True
+        if not n:
+            return False
