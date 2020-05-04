1eb1af3c0ab54f483700161600a21e846cff9ca6
XiakaiPan <13212017962@163.com>
2020-05-04 11:27:16 +0800

    rename directory to week*_dayXX..XX

--- CS61B_SP2019/week4_day35363839/proj1b/OffByOne.java
+++
@@ -1,7 +0,0 @@
-public class OffByOne implements CharacterComparator {
-    @Override
-    public boolean equalChars(char x, char y) {
-        // return (Math.abs((int) x - (int) y) == 1);
-        return x - y == 1 || y - x == 1;
-    }
-}
--- CS61C_SP2015/week4_day38/lab3/lab3_ex1.asm
+++
@@ -1,9 +0,0 @@
-#lab3_ex1.s
-#The lab3_ex*.s are not avaliable, try disc2 in Mars.
-
-#2.1
-
-
-lw $t0, 12($s0)
-add $t1, $t0, $t0
-sw $t0, 4($t1)
