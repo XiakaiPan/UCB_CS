ff57e06e871faadc856b07ae0eada4e1c6c6fa0b
XiakaiPan <13212017962@163.com>
2020-05-10 23:10:35 +0800

    week5 files added

---
+++ CS61B_SP2019/week6_day46/lab6/QuickFindDS.java
@@ -0,0 +1,29 @@
+public class QuickFindDS implements DisjointSets {
+    private int[] id;
+
+    /** theta(N) */
+    public QuickFindDS(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+
+    @Override
+    /** need to iterate through the array => theta(N) */
+    public void connect(int p, int q) {
+        int pid = id[p];
+        int qid = id[q];
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pid) {
+                id[i] = qid ;
+            }
+        }
+    }
+
+    @Override
+    /** theta(1) */
+    public boolean isConnected(int p, int q) {
+        return (id[p] == id[q]);
+    }
+}
